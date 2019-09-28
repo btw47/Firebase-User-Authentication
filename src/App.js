2911384c71a3c24d0c04709ec81c3ba0c1a7fc03
Bradley Wong <btw47@nau.edu>
2019-09-28 11:12:53 -0700

    Add UsersComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import HomeComponent from "./components/Home";
+import UsersComponent from "./components/Users";
 import LoginComponent from "./components/Login";
 import RegisterComponent from "./components/Register";
-import UsersComponent from "./components/Users";
-import HomeComponent from "./components/Home";
+
+const HOME_ROUTE = '/';
+const LOGIN_ROUTE = '/login';
+const REGISTER_ROUTE = '/register';
+const USERS_ROUTE = '/users';
 
 export default function App() {
   return (
@@ -13,16 +18,16 @@ export default function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to={HOME_ROUTE}>Home</Link>
             </li>
             <li>
-              <Link to="/login">Login</Link>
+              <Link to={LOGIN_ROUTE}>Login</Link>
             </li>
             <li>
-              <Link to="/register">Register</Link>
+              <Link to={REGISTER_ROUTE}>Register</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to={USERS_ROUTE}>Users</Link>
             </li>
           </ul>
         </nav>
@@ -30,16 +35,16 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/login">
+          <Route path={LOGIN_ROUTE}>
             <LoginComponent />
           </Route>
-          <Route path="/Register">
+          <Route path={REGISTER_ROUTE}>
             <RegisterComponent />
           </Route>
-          <Route path="/Users">
+          <Route path={USERS_ROUTE}>
             <UsersComponent />
           </Route>
-          <Route path="/">
+          <Route path={HOME_ROUTE}>
             <HomeComponent />
           </Route>
         </Switch>
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,89 @@
 import React from "react";
 
+const USER_ROLES = [
+    {
+        id: 'admin',
+        label: 'Administrator'
+    },
+    {
+        id: 'staff',
+        label: 'Staff'
+    },
+    {
+        id: 'client',
+        label: 'Client'
+    },
+]
+
 export default class UsersComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [
+                {
+                    id: '1',
+                    name: 'Test Name 1',
+                    email: 'Test Email 1',
+                    role: USER_ROLES[0].id
+                },
+                {
+                    id: '2',
+                    name: 'Test Name 2',
+                    email: 'Test Email 2',
+                    role: USER_ROLES[1].id
+                },
+                {
+                    id: '3',
+                    name: 'Test Name 3',
+                    email: 'Test Email 3',
+                    role: USER_ROLES[2].id
+                }
+            ]
+        }
+    }
+
+    getUsers() {
+        if (!this.state.users || this.state.users.length === 0) return <h2>No users found.</h2>
+        return this.state.users.map(user => {
+            return (
+                <div key={user.id}>
+                    Name: <span>{user.name}</span> |
+                    Email: <span>{user.email}</span> |
+                    Role: {this.getRole(user)}
+
+                    <button onClick={e => this.updateUser(user, e)}>Update</button>
+                </div>
+            );
+        })
+    }
+
+    getRole(user) {
+        return (
+            <select defaultValue={user.role} onChange={this.updateState}>
+                {USER_ROLES.map(role => {
+                    return <option value={role.id} key={role.id}>{role.label}</option>
+                })}
+            </select>
+        )
+    }
+
+    updateState(e) {
+        console.log(e.target)
+        console.log(e.target.value)
+    }
+
+    updateUser(user, e) {
+        e.preventDefault()
+        console.log(user)
+    }
+
     render() {
         return (
-            <h2>
-                Users
-            </h2>
+            <div>
+                <h2>Users</h2>
+                <ul>{this.getUsers()}</ul>
+            </div>    
         )
     }
 }
