0abd7e2b71c8e32f34483a21e2a0b6fc91fd98d6
Bradley Wong <btw47@nau.edu>
2019-09-28 10:38:36 -0700

    Add login and register forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,44 @@
 import React from "react";
 
 export default class LoginComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    
+    handleSubmit(event) {
+        console.log(this.state)
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <h2>
-                Login
-            </h2>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Email:
+                    <input type="text" value={this.state.email} onChange={this.handleChange} name="email" />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={this.state.password} onChange={this.handleChange} name="password" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         )
     }
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,54 @@
 import React from "react";
 
 export default class RegisterComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirm: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    
+    handleSubmit(event) {
+        console.log(this.state)
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <h2>
-                Register
-            </h2>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
+                </label>
+                <label>
+                    Email:
+                    <input type="text" value={this.state.email} onChange={this.handleChange} name="email" />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={this.state.password} onChange={this.handleChange} name="password" />
+                </label>
+                <label>
+                    Confirm Password:
+                    <input type="password" value={this.state.passwordConfirm} onChange={this.handleChange} name="passwordConfirm" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         )
     }
 }
